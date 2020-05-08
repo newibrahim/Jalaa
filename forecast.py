8e379b07babd6df9aecaca6a524fdea43b1a46e6
Ibrahim <dreamunlimited1234@gmail.com>
2020-05-08 18:36:24 -0500

    second homework commit

--- forecast.py
+++ forecast.py
@@ -3,8 +3,7 @@ owm = pyowm.OWM('0741d86c5a4615231dd18ef3c8e280ed')
 location = owm.weather_at_place('New York, US')
 weather = location.get_weather()
 
-temp = weather.get_temperature('celsius')
-print(temp)
+temp = weather.get_temperature('fahrenheit')
 header = "\n\tNEW YORK 3-DAY FORECAST:\n\n\tDay\t\tHigh\tLow\tConditions\n\t---\t\t----\t---\t----------\n"
-forecast = "\tSunday\t\t" + str(temp.get('temp_max')) + "C" + "\t" + str(temp.get('temp_min')) + "C" + "\tSunny\n\tMonday\t\t69F\t57F\tSunny\n\tTuesday\t\t71F\t50F\tCloudy\n"
+forecast = "\tSunday\t\t" + str(temp.get('temp_max')) + "F" + "\t" + str(temp.get('temp_min')) + "F" + "\tSunny\n\tMonday\t\t" + str(temp.get('temp_max')) + "F" + "\t" + str(temp.get('temp_min')) + "F" + "\tSunny\n\tTuesday\t\t" + str(temp.get('temp_max')) + "F" + "\t50F\tCloudy\n"
 print(header + forecast)
