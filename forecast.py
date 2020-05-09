4f810e61e66a16e921a8ca0dc4165e0095478443
Ibrahim <dreamunlimited1234@gmail.com>
2020-05-09 15:06:58 -0500

    third commit homework

--- forecast.py
+++ forecast.py
@@ -1,9 +1,12 @@
 import pyowm
+import datetime
+from datetime import date
+import calender
 owm = pyowm.OWM('0741d86c5a4615231dd18ef3c8e280ed')
 location = owm.weather_at_place('New York, US')
 weather = location.get_weather()
 
 temp = weather.get_temperature('fahrenheit')
 header = "\n\tNEW YORK 3-DAY FORECAST:\n\n\tDay\t\tHigh\tLow\tConditions\n\t---\t\t----\t---\t----------\n"
-forecast = "\tSunday\t\t" + str(temp.get('temp_max')) + "F" + "\t" + str(temp.get('temp_min')) + "F" + "\tSunny\n\tMonday\t\t" + str(temp.get('temp_max')) + "F" + "\t" + str(temp.get('temp_min')) + "F" + "\tSunny\n\tTuesday\t\t" + str(temp.get('temp_max')) + "F" + "\t50F\tCloudy\n"
+forecast = "\t" + calendar.day_name[datetime.datetime.today().weekday()] + "\t" + str(temp.get('temp_max')) + "F" + "\t" + str(temp.get('temp_min')) + "F" + "\t" + calendar.day_name[datetime.datetime.today().weekday() + 1] + "\n\t" + calendar.day_name[datetime.datetime.today().weekday() + 2] + "\t\t" + str(temp.get('temp_max')) + "F" + "\t" + str(temp.get('temp_min')) + "F" + "\tSunny\n\tTuesday\t\t" + str(temp.get('temp_max')) + "F" + "\t50F\tCloudy\n"
 print(header + forecast)
